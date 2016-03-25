1bcbc566aa444fd3f35ce440ef18cf1771784580
lanou3g <975157026@qq.com>
2016-03-25 14:20:49 +0800

    verson1.0

---
+++ testNew/testNew/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testNew
+//
+//  Created by lanou3g on 16/3/25.
+//  Copyright © 2016年 oceanli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
